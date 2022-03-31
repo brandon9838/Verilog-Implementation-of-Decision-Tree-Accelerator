19d684e85653664d3c55271b19914e4af604d26f
brandon9838 <55751452+brandon9838@users.noreply.github.com>
2022-03-31 21:02:01 +0200

    Update DEC.v

--- hardware/RTL/DEC.v
+++ hardware/RTL/DEC.v
@@ -40,7 +40,7 @@ input [11:0] input_ID;
 
 output input_ready;
 output reg out_valid;
-output [7:0] out_ID;
+output [11:0] out_ID;
 output reg out;
 
 //Memory
@@ -190,7 +190,6 @@ always@(*)begin
 	if((&input_mode_s1_r) && !(|node_s1_r[7:1]))begin
 		out_valid_s2_w=1;
 		out_s2_w=node_s1_r[0];
-		input_ID_s2_w=0;
 	end
 	
 	input_ready_s2_w=((&input_mode_s1_r) && (|node_s1_r[7:1]))?0:1;
